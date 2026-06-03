f70b0765c73c973d8868551cfddbc805856b4170
fatu-code <fatumeshariff@gmail.com>
2026-06-04 00:47:13 +0300

    hmmm

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -112,6 +112,12 @@ const NAV_ITEMS = [
     href: "reports.html",
     icon: '<svg viewBox="0 0 16 16"><path d="M4 1.5h6l3 3V14a.5.5 0 01-.5.5h-9A.5.5 0 013 14V2a.5.5 0 01.5-.5z"/><path d="M5.5 8h5M5.5 10.5h5M5.5 5.5h3"/></svg>',
   },
+  {
+    id: "staff",
+    label: "Staff",
+    href: "users.html",
+    icon: '<svg viewBox="0 0 16 16"><circle cx="5.5" cy="5" r="2.3"/><path d="M1.5 13c0-2.4 1.8-4 4-4s4 1.6 4 4"/><circle cx="11.5" cy="5.5" r="1.8"/><path d="M10 9.2c2 .1 3.4 1.5 3.4 3.8"/></svg>',
+  },
 ];
 
 function buildSidebar(active) {
